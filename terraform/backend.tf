8acb7725a45febfd619a37f01fa7fdccf15e7a8e
Vaibhav <vaibhavnalla15@gmail.com>
2026-08-03 20:34:23 +0530

    feat(terraform): establish Terraform foundation

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "tf-enterprise-data-processing-platform "
+    key          = "enterprise-data-processing-platform/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region for all resources"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
